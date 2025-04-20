0a4cf8e68b9c585971ddef826b26fcd683bde914
Yaarabi <yaarabi318@gmail.com>
2025-04-20 20:00:13 +0100

    add the some of retouch in file.js of teacher and student

--- Techer/teacher.js
+++ Techer/teacher.js
@@ -1,2 +1,30 @@
-import { db } from '../home/home';
-console.log(db);
+// import { db } from '/home/home.js';
+// console.log(db);
+// export let nameOfteacher = 555
+
+// document.getElementById('teacherName')
+let menu = document.getElementById('dropMenu')
+let menuIcon = document.getElementById('menuBar')
+menuIcon.addEventListener('click', ()=>{
+    menu.style.display='flex'
+})
+
+let closeBar = document.getElementById('close')
+closeBar.addEventListener('click', (e)=>{
+    
+    
+    menu.style.display='none'      
+    
+})
+let out = document.getElementById('out')
+out.addEventListener('click', ()=>{
+    window.location.href = '/home/home.html';
+})
+let boite = document.getElementById('boitIcon')
+let message = document.getElementById('boite')
+boite.addEventListener('click', ()=>{
+    message.style.visibility='visible'
+})
+message.addEventListener('click', ()=>{
+    message.style.visibility='hidden'
+})
--- student/stud.js
+++ student/stud.js
@@ -1,2 +1,27 @@
-import { db } from '../home/home';
-console.log(db);
+// import { db } from '../home/home';
+// console.log(db);
+let sideBar = document.getElementById('sideBar')
+let menuIcon = document.getElementById('barIcon')
+menuIcon.addEventListener('click', ()=>{
+    sideBar.style.display='flex'
+})
+
+let closeBar = document.getElementById('closes')
+closeBar.addEventListener('click', (e)=>{
+    
+    
+    sideBar.style.display='none'      
+    
+})
+let out = document.getElementById('outs')
+out.addEventListener('click', ()=>{
+    window.location.href = '/home/home.html';
+})
+let boite = document.getElementById('messagIcon')
+let message = document.getElementById('messag')
+boite.addEventListener('click', ()=>{
+    message.style.visibility='visible'
+})
+message.addEventListener('click', ()=>{
+    message.style.visibility='hidden'
+})
